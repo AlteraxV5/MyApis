cb396b51444e0f2feeedb29ae9c95a7a1b87ce5d
AltOffc!? Dev <altstar2921@gmail.com>
2026-03-14 10:46:10 +0700

    Update Thdl.js

--- router/download/Thdl.js
+++ router/download/Thdl.js
@@ -61,3 +61,40 @@ async function threadsdl(url) {
     media
   }
 }
+
+module.exports = async function handler(req, res) {
+
+  const url = req.query?.url || req.body?.url
+
+  if (!url) {
+    return res.status(400).json({
+      status: false,
+      creator: "AltOffx",
+      message: "Parameter 'url' diperlukan."
+    })
+  }
+
+  try {
+
+    const data = await threadsdl(url)
+
+    res.json({
+      status: true,
+      creator: "AltOffx",
+      result: data
+    })
+
+  } catch (error) {
+
+    res.status(500).json({
+      status: false,
+      creator: "AltOffx",
+      message: error.message
+    })
+
+  }
+
+}
+
+/* supaya bisa dipakai juga sebagai function */
+module.exports.threadsdl = threadsdl
